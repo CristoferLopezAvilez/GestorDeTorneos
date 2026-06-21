ca727edccc47d59007ca14625d319c9d7caaccf2
Cristofer Lopez Avilez <nosoynirvana@gmail.com>
2026-06-21 18:32:37 -0300

    test unitarios

--- DominioTesting/RoundRobinGeneratorShould.cs
+++ DominioTesting/RoundRobinGeneratorShould.cs
@@ -3,23 +3,28 @@ using Xunit;
 
 namespace DominioTesting
 {
-   
+
     public class RoundRobinGeneratorShould
     {
         [Fact]
         public void Generar_RondasCorrectas()
         {
             // Arrange
-          var jugadores = new List<Jugador>
+            var jugadores = new List<Jugador>
             {
                 new Jugador { NombreCompleto = "A" },
                 new Jugador { NombreCompleto = "B" },
                 new Jugador { NombreCompleto = "C" },
                 new Jugador { NombreCompleto = "D" }
             };
+
+            var generador = new RoundRobinGenerator();
+
             // Act
-            var rondas = RoundRobinGenerator.Generar(jugadores);
+            List<Ronda> rondas = generador.Generar(jugadores);
+
+            // Assert
+            Assert.Equal(3, rondas.Count);
         }
-        
     }
 }
